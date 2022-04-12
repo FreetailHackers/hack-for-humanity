bdf549022ae52b378d4c9199bf995de81746b871
Ryan Hou <rhou7873@gmail.com>
2022-04-12 13:44:08 -0500

    added penguin to home page

--- info-site/index.js
+++ info-site/index.js
@@ -1,6 +1,6 @@
 var down = false;
 function pullDownSchedule() {
-    var targetDiv = document.getElementById("schedule-div");
+    var targetDiv = document.getElementById("map-div");
     if (!down) {
         targetDiv.classList.remove("div-animation-up");
         targetDiv.classList.add("div-animation-down");
